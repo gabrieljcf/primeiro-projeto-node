309964f28ddb16fc4cb06cb6c7dd21390a30e885
Gabriel Nogueira <gabrieljcf@hotmail.com>
2020-08-31 22:00:08 -0300

    adding editorconfig

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello GoStack' });
+  return response.json({ message: 'Hello GoStack' });
 })
 
 app.listen(3333, () => {
-    console.log('🚀 Server started on port 3333');
+  console.log('🚀 Server started on port 3333');
 });
